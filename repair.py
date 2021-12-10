59830a9f080d3a1cc09e30015685a47b5a5f9e5f
Zoe Martin <git@zoemartin.me>
2021-12-10 16:02:48 +0100

    add replacement option

--- repair.py
+++ repair.py
@@ -1,10 +1,19 @@
 import sys
 import re
+import json
 
 pattern = re.compile(r'(?:begin{document})([\s\S]*)(?:\\end{document})')
+
+j = open('../../replace.json')
+json_data = json.load(j)
 
 with open(str(sys.argv[1]), 'r+') as f:
     lines = f.readlines()
     s = "".join(lines)
+
+    for i in json_data["replace"]:
+        print(i["replace"])
+        s = s.replace(i["search"], i["replace"])
+
     with open(str(sys.argv[1]), 'w') as g:
         g.writelines(pattern.findall(s))
