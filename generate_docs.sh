e833a3c1e1362d446657ee124fd4862d923e3190
Zoe Martin <git@zoemartin.me>
2021-12-10 15:35:14 +0100

    updates

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,12 +3,16 @@
 mkdir -p docs/latex
 mkdir -p docs/md
 
-# # Transpile source
-# echo "Transpiling Typescript source"
-# tsc -p backend/tsconfig.json &>/dev/null
-# tsc -p frontend/tsconfig.json &>/dev/null
+echo "Updating submodules..."
+git submodule update --recursive --remote
 
-echo "Generating Markdown Docs"
+
+# Transpile source
+echo "Transpiling Typescript source..."
+tsc -p backend/tsconfig.json &>/dev/null
+tsc -p frontend/tsconfig.json &>/dev/null
+
+echo "Generating Markdown Docs..."
 jsdoc2md --files backend/dist/models/**/*.js > docs/md/models.backend.md
 jsdoc2md --files backend/dist/controllers/**/*.js > docs/md/controllers.backend.md
 jsdoc2md --files backend/dist/types/**/*.js > docs/md/types.backend.md
@@ -17,7 +21,7 @@ jsdoc2md --files frontend/dist/out-tsc/app/components/**/*.js > docs/md/view.fro
 jsdoc2md --files frontend/dist/out-tsc/app/services/**/*.js > docs/md/services.frontend.md
 jsdoc2md --files frontend/dist/out-tsc/app/types/**/*.js > docs/md/types.frontend.md
 
-echo "Generating LaTeX Docs"
+echo "Generating LaTeX Docs..."
 cd docs/md
 for f in *.md; do
     pandoc --from=markdown --output=../latex/$f.tex $f --to=latex --standalone
