db1ffa5848b1a3cba1e67c93100e69e712b8b12c
Zoe Martin <git@zoemartin.me>
2021-12-11 16:17:53 +0100

    updates

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,26 +4,28 @@ mkdir -p docs/latex
 mkdir -p docs/md
 
 echo "Updating submodules..."
-git submodule update --recursive --remote
+#git submodule update --recursive --remote
 
 
 # Transpile source
 echo "Transpiling Typescript source..."
-tsc -p backend/tsconfig.json &>/dev/null
-tsc -p frontend/tsconfig.json &>/dev/null
+#tsc -p backend/tsconfig.json &>/dev/null
+#tsc -p frontend/tsconfig.json &>/dev/null
 
 echo "Generating Markdown Docs..."
-jsdoc2md --files backend/dist/models/**/*.js > docs/md/models.backend.md
-jsdoc2md --files backend/dist/controllers/**/*.js > docs/md/controllers.backend.md
-jsdoc2md --files backend/dist/types/**/*.js > docs/md/types.backend.md
+options=""
+jsdoc2md $options --files backend/dist/models/**/*.js > docs/md/models.backend.md
+jsdoc2md $options --files backend/dist/controllers/**/*.js > docs/md/controllers.backend.md
+jsdoc2md $options --files backend/dist/types/**/*.js > docs/md/types.backend.md
 
-jsdoc2md --files frontend/dist/out-tsc/app/components/**/*.js > docs/md/view.frontend.md
-jsdoc2md --files frontend/dist/out-tsc/app/services/**/*.js > docs/md/services.frontend.md
-jsdoc2md --files frontend/dist/out-tsc/app/types/**/*.js > docs/md/types.frontend.md
+jsdoc2md $options --files frontend/dist/out-tsc/app/components/**/*.js > docs/md/view.frontend.md
+jsdoc2md $options --files frontend/dist/out-tsc/app/services/**/*.js > docs/md/services.frontend.md
+jsdoc2md $options --files frontend/dist/out-tsc/app/types/**/*.js > docs/md/types.frontend.md
 
 echo "Generating LaTeX Docs..."
 cd docs/md
 for f in *.md; do
+    python3 ../../repair.py -md $f
     pandoc --from=markdown --output=../latex/$f.tex $f --to=latex --standalone
-    python3 ../../repair.py ../latex/$f.tex
+    python3 ../../repair.py -latex ../latex/$f.tex
 done
--- repair.py
+++ repair.py
@@ -7,12 +7,22 @@ pattern = re.compile(r'(?:begin{document})([\s\S]*)(?:\\end{document})')
 j = open('../../replace.json')
 json_data = json.load(j)
 
-with open(str(sys.argv[1]), 'r+') as f:
+with open(str(sys.argv[2]), 'r+') as f:
     lines = f.readlines()
     s = "".join(lines)
 
-    for i in json_data["regex"]:
-        s = re.sub(i["search"], i["replace"], s)
+    if (sys.argv[1] == '-latex'):
+        for i in json_data["latex_regex"]:
+            s = re.sub(i["search"], i["replace"], s)
 
-    with open(str(sys.argv[1]), 'w') as g:
-        g.writelines(pattern.findall(s))
+        with open(str(sys.argv[2]), 'w') as g:
+            g.writelines(pattern.findall(s))
+    else:
+        for i in json_data["md_lower"]:
+            s = re.sub(i, lambda m: m.group(0).lower(), s)
+        for i in json_data["md_regex"]:
+            s = re.sub(i["search"], i["replace"], s)
+        with open(str(sys.argv[2]), 'w') as g:
+            g.write(s)
+
+    
